5d39c1a305a00cf81a5b6bc564a20c36ec793d27
Casper Björk <erik.casper.bjork@gmail.com>
2017-02-20 12:02:46 +0100

    Add project files.

---
+++ Gastbok/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gastbok.Startup))]
+namespace Gastbok
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
